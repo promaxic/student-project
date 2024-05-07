598998d1902a6f6aacb122a219aa24671ad5ba46
ProMaxic <ProMaxic@gmail.com>
2024-05-08 00:45:45 +0300

    Nothing

--- src/main/java/edu/javacourse/studentorder/SaveStudentOrder.java
+++ src/main/java/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -6,10 +6,10 @@ public class SaveStudentOrder {
     public static void main(String[] args) {
 
         StudentOrder so = new StudentOrder();
-        so.hFirstName = "Alexsey";
-        so.hLastName = "Petrov";
-        so.wFirstName = "Galina";
-        so.wLastName = "Petrova";
+        so.sethFirstName("Alexsey");
+        so.sethLastName("Petrov");
+        so.setwFirstName("Galina");
+        so.setwLastName("Petrova");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -17,7 +17,7 @@ public class SaveStudentOrder {
     }
     static long saveStudentOrder(StudentOrder studentOrder){
         long answer = 199;
-        System.out.println("saveStudentOrder" + studentOrder.hLastName);
+        System.out.println("saveStudentOrder" + studentOrder.gethLastName());
 
         return answer;
     }
